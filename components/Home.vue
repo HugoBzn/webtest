e55d7aac8a826082711149884f426f2f8f672a36
HugoBzn <hugobazan1499@gmail.com>
2022-10-19 10:40:22 -0500

    New position cards

--- components/Home.vue
+++ components/Home.vue
@@ -45,29 +45,6 @@
     <br />
 
     <div class="max-w-sm bg-gray-800 rounded-lg border border-gray-700 shadow-md">
-      <div class="w-full h-full">
-        <iframe class="w-full" src="https://www.youtube.com/embed/qJa-VFwPpYA" style="height: 215px;"></iframe>
-      </div>
-      <div class="p-5">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
-          Page users
-        </h5>
-        <p class="mb-3 font-normal text-gray-400">
-          Page where you can see all the registered users.
-        </p>
-        <div class="center">
-          <NuxtLink
-            to="/users"
-            class="transition duration-300 ease-in-out centertext-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-            > Go to page!
-          </NuxtLink>
-          <br />
-        </div>
-      </div>
-    </div>
-    <br />
-
-    <div class="max-w-sm bg-gray-800 rounded-lg border border-gray-700 shadow-md">
       <img class="rounded-t-lg" src="@/assets/images/h.png" alt="" />
       <div class="p-5">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
@@ -102,6 +79,29 @@
         <div class="center">
           <NuxtLink
             to="/"
+            class="transition duration-300 ease-in-out centertext-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            > Go to page!
+          </NuxtLink>
+          <br />
+        </div>
+      </div>
+    </div>
+    <br />
+
+    <div class="max-w-sm bg-gray-800 rounded-lg border border-gray-700 shadow-md">
+      <div class="w-full h-full">
+        <iframe class="w-full" src="https://www.youtube.com/embed/qJa-VFwPpYA" style="height: 215px;"></iframe>
+      </div>
+      <div class="p-5">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
+          Page users
+        </h5>
+        <p class="mb-3 font-normal text-gray-400">
+          Page where you can see all the registered users.
+        </p>
+        <div class="center">
+          <NuxtLink
+            to="/users"
             class="transition duration-300 ease-in-out centertext-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
             > Go to page!
           </NuxtLink>
