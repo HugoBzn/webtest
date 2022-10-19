c5cad0b416ecfda62feb12118cc8adbf2eecd083
HugoBzn <hugobazan1499@gmail.com>
2022-10-19 13:40:40 -0500

    Working using fetch and options api

--- pages/info.vue
+++ pages/info.vue
@@ -1,15 +1,13 @@
 <template>
-    <div class="grid lg:grid-cols-3 sm:grid-cols-1 gap-4 place-content-center">
-        <Info v-for="user in users" :username="user"/>
-    </div>
+  <div class="grid lg:grid-cols-3 sm:grid-cols-1 gap-4 place-content-center">
+    <Info v-for="user in users" :username="user" />
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const users = ref(['1', "2"]);
+const users = ref(['1', '2', '7']);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
